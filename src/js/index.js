21b08112efe3c7970f7e319b9d7834c5dbaaf437
Luke Underwood <moresilenter@gmail.com>
2018-03-03 00:53:45 -0800

    Yay

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-console.log(`I'm a boilerplate :-)`);
+import React from "react";
+import { render } from "react-dom";
+import store from "./store";
+import { addArticle } from "./actions";
+
+window.store = store;
+window.addArticle = addArticle;
+
+const App = () => <h1>Setting up React with Parcel bundler</h1>;
+const wrapper = document.getElementById("app");
+
+wrapper ? render(<App />, wrapper) : false;
---
+++ src/js/reducers/index.js
@@ -0,0 +1,17 @@
+import ADD_ARTICLE from "../constants/action-types";
+
+const initialState = {
+  articles: []
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      state.articles.push(action.payload);
+      return { ...state, articles: [...state.articles, action.payload] };
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
